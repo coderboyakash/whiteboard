622d1419cf08c59417e628b8ede41541ce5f1b54
coderboyakash <akash.chowdhury@weavers-web.com>
2021-10-01 21:15:24 +0530

    finally done

--- index.js
+++ index.js
@@ -12,15 +12,7 @@ server.listen(3000, () => {
 let connections = []
 
 io.on('connect', (socket) => {
-    // console.log(`connected ${socket.id}`)
     connections.push(socket)
-    // socket.on('draw', (data) => {
-    //     connections.forEach(con => {
-    //         if(con.id !== socket.id){
-    //             con.emit('ondraw', {x: data.x, y: data.y})
-    //         }
-    //     })
-    // })
     socket.on('down', (data) => {
         connections.forEach(con => {
             if(con.id !== socket.id){
