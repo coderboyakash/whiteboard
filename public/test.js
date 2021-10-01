35d7e2cf2bc35a4b90eabaac68e09811f87094e9
coderboyakash <akash.chowdhury@weavers-web.com>
2021-10-01 19:46:47 +0530

    working one size eraser and size property

--- public/test.js
+++ public/test.js
@@ -60,7 +60,6 @@ socket.on('setMouseMove', ({x, y}) => {
 socket.on('setMouseDown', ({x, y}) => {
     mouse.x = x;
     mouse.y = y;
-    // tmp_canvas.addEventListener('mousemove', onPaint, false);
     ppts.push({x: mouse.x, y: mouse.y});
     onPaint();
 })
@@ -121,7 +120,6 @@ const changeEraserSize = (e) => {
 }
 
 tmp_canvas.addEventListener('mousedown', function(e) {
-    // console.log('down ');
     tmp_canvas.addEventListener('mousemove', onPaint, false);
     
     mouse.x = typeof e.offsetX !== 'undefined' ? e.offsetX : e.layerX;
@@ -146,16 +144,11 @@ tmp_canvas.addEventListener('mouseup', function() {
 }, false);
 
 var onPaint = function() {
-    // console.log('painting');
-    
     // Saving all the points in an array
     ppts.push({x: mouse.x, y: mouse.y});
-    // console.log(ppts);
     if (ppts.length < 3) {
         var b = ppts[0];
         tmp_ctx.beginPath();
-        //ctx.moveTo(b.x, b.y);
-        //ctx.lineTo(b.x+50, b.y+50);
         tmp_ctx.arc(b.x, b.y, tmp_ctx.lineWidth / 2, 0, Math.PI * 2, !0);
         tmp_ctx.fill();
         tmp_ctx.closePath();
@@ -164,7 +157,6 @@ var onPaint = function() {
     }
     
     // Tmp canvas is always cleared up before drawing.
-    // console.log('clear called', tmp_canvas.width, tmp_canvas.height);
     tmp_ctx.clearRect(0, 0, tmp_canvas.width, tmp_canvas.height);
     
     tmp_ctx.beginPath();
@@ -173,12 +165,10 @@ var onPaint = function() {
     for (var i = 1; i < ppts.length - 2; i++) {
         var c = (ppts[i].x + ppts[i + 1].x) / 2;
         var d = (ppts[i].y + ppts[i + 1].y) / 2;
-        // console.log(c, d);
         tmp_ctx.quadraticCurveTo(ppts[i].x, ppts[i].y, c, d);
     }
     
     // For the last 2 points
-    // console.log('last two point', ppts[i].x, ppts[i].y);
     tmp_ctx.quadraticCurveTo(
         ppts[i].x,
         ppts[i].y,
